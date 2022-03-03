320930bcaeabd36c794131acdbe2d8ea9081814d
Ivan comp <ivan.veselnikov@gmail.com>
2022-03-03 22:48:30 +0300

    create permission v1.1

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,7 +2,15 @@ from rest_framework.permissions import (
     BasePermission, SAFE_METHODS)
 
 
-class IsAuthorOrReadOnly(BasePermission):
+class UserPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
+
+
+class ModeratorPermission(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if user.role == 'Moderator':
+            return True
