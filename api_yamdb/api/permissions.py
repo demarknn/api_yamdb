17c0270d6d34b052f5a53f3b2a178b8ac7cd45c0
Ivan comp <ivan.veselnikov@gmail.com>
2022-03-07 12:31:32 +0300

    pass 29+cvs+ adminorread

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -67,3 +67,15 @@ class IsAuthorOrModeratorOrAdminOrReadOnly(BasePermission):
             and user.is_moderator
 
         )
+
+
+class AdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_admin
